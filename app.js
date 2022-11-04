73310a614b118fdbfb2bf9e18e3d865a336302be
zivernay <39434401+zivernay@users.noreply.github.com>
2022-11-04 13:50:11 +0300

    Add play function to diplay player moves

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const Player = function(marker, name="player 1") {
     const _name = name;
     const _marker = marker;
-    const _turn = false;
+    let _turn = [false];
     const _cells = [];
-    const play = () => {
-        console.log("I played")
+    const play = (index) => {
+        Board.grid[index][2] = _marker;
+        _cells.push(Board.grid[index]);
+        _turn[0] = false;
     };
-    const getPlayerName = () => name;
-    return {getPlayerName, play}
+    const getPlayerName = () => _name;
+    const getPlayerMarker = () => _marker
+    const isPlayerTurn = () => _turn[0];
+    const togglePlayerTurn = () => {_turn[0] = true};
+    return {play, getPlayerName, isPlayerTurn,togglePlayerTurn, getPlayerMarker}
 }
 
 const Board = (function(size=3) {
@@ -30,25 +35,44 @@ const Board = (function(size=3) {
         }
         return count
     };
-    const isFreeCell = (cell, index=2) => {
-        return (cell[2])? true: false
-    };
-    return {getFreeCellCount, isFreeCell}
+    const isFreeCell = (cell, index=2) => !(cell[index]);
+    return {grid, getFreeCellCount, isFreeCell}
 })();
 
 const ticTacToe = (function(Player, Board) {
     const player1 = Player("O", "P1");
     const player2 = Player("X", "P2");
-    
-    const domCache = {
+
+    const _domCache = {
         grid: document.querySelector('#grid'),
+        tds : grid.querySelectorAll("td"),
     };
 
-    const render = function(target, content) {
+    const _render = function(target, content) {
         target.textContent = content
     };
-
-    const play = function() {
-
+    const _getPlayer = function() {
+        if (player1.isPlayerTurn()) {
+            player2.togglePlayerTurn()
+            return player1
+        } else {
+            player1.togglePlayerTurn()
+            return player2
+        }
     }
-})()
+
+    const play = function(cellElem) {
+        const cellIndex = parseInt(cellElem.dataset["cellIndex"]);
+        const cell = Board.grid[cellIndex];
+        if (!(Board.isFreeCell(cell))) {
+            return
+        }
+        const player = _getPlayer();
+        player.play(cellIndex);
+        _render(cellElem, player.getPlayerMarker());
+    };
+
+    _domCache.tds.forEach(element => {
+       element.addEventListener("click", (e)=>{play(e.target)})
+    });
+})(Player, Board)
