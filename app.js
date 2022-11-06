1e5c9c56b036650514f1823158b2a315d1f0ce59
zivernay <39434401+zivernay@users.noreply.github.com>
2022-11-06 20:36:09 +0300

    Add win or lose logic

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const Player = function(marker, name="player 1") {
     const _marker = marker;
     let _turn = [false];
     const _cells = [];
-    const play = (index) => {
-        Board.grid[index][2] = _marker;
+    const play = (index, cellElem) => {
+        Board.grid[index][2] = cellElem;
         _cells.push(Board.grid[index]);
         _turn[0] = false;
     };
@@ -12,7 +12,15 @@ const Player = function(marker, name="player 1") {
     const getPlayerMarker = () => _marker
     const isPlayerTurn = () => _turn[0];
     const togglePlayerTurn = () => {_turn[0] = true};
-    return {play, getPlayerName, isPlayerTurn,togglePlayerTurn, getPlayerMarker}
+    const getCells = () => _cells;
+    return {
+         play,
+         getPlayerName,
+         isPlayerTurn,
+         togglePlayerTurn,
+         getPlayerMarker,
+         getCells,
+        }
 }
 
 const Board = (function(size=3) {
@@ -26,31 +34,40 @@ const Board = (function(size=3) {
         return grid
     };
     const grid = getGrid(size);
-    const getFreeCellCount = (index=2) => {
-        const count = 0;
+    const getFreeCell = (index=2) => {
+        const cells = [];
         for (let arr of grid) {
-            if(arr[index] === ""){
-                count++;
+            if(arr[index] === null){
+                cells.push(arr);
             }
         }
-        return count
+        return cells
     };
     const isFreeCell = (cell, index=2) => !(cell[index]);
-    return {grid, getFreeCellCount, isFreeCell}
+    return {
+         grid,
+         getFreeCell,
+         isFreeCell,
+        }
 })();
 
 const ticTacToe = (function(Player, Board) {
+    //Create players for a 2 player game
     const player1 = Player("O", "P1");
     const player2 = Player("X", "P2");
 
+    //Select dom elements that are required for manipulation
     const _domCache = {
         grid: document.querySelector('#grid'),
         tds : grid.querySelectorAll("td"),
     };
 
+    //Display  player moves in the grid
     const _render = function(target, content) {
         target.textContent = content
     };
+
+    //Return the player depending on whose turn it is to play
     const _getPlayer = function() {
         if (player1.isPlayerTurn()) {
             player2.togglePlayerTurn()
@@ -61,6 +78,7 @@ const ticTacToe = (function(Player, Board) {
         }
     }
 
+    // Player move
     const play = function(cellElem) {
         const cellIndex = parseInt(cellElem.dataset["cellIndex"]);
         const cell = Board.grid[cellIndex];
@@ -68,9 +86,62 @@ const ticTacToe = (function(Player, Board) {
             return
         }
         const player = _getPlayer();
-        player.play(cellIndex);
+        player.play(cellIndex, cellElem);
         _render(cellElem, player.getPlayerMarker());
-    };
+        if (player.getCells().length > 2) {
+            endGame(player.getCells())
+        }
+    }
+
+
+    const _linearEquations = function(arr) {
+        for (let i = 0; i < 3; i++) {
+            let cx = 0,
+                cy = 0,
+                cxy = 0,
+                cyx = 0;
+            const n = 2;
+            for (let [x, y] of arr) {
+                switch (true) {
+                    case (x == i):
+                        cx++;
+                        if (cx > n) {
+                            return true
+                        }
+                        break;
+
+                    case (y == i):
+                        cy++;
+                        if (cy > n) {
+                            return true
+                        }
+                        break;
+
+                    case (y == x):
+                        cxy++;
+                        if (cxy > n) {
+                            return true
+                        }
+                        break;
+
+                    case (y == (-x + 3)):
+                        cxy++;
+                        if (cyx > n) {
+                            return true
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    const endGame = function (arr) {
+        if (_linearEquations(arr)) {
+            console.log(`won`)
+        } else  {
+            console.log("draw")
+        }
+    }
 
     _domCache.tds.forEach(element => {
        element.addEventListener("click", (e)=>{play(e.target)})
