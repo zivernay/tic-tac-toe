4cc1a28cde2419d3d3c3594d797b76d4001882b1
zivernay <39434401+zivernay@users.noreply.github.com>
2022-11-12 09:17:36 +0300

    Add delay and stop game after win or draw

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Player = function(marker, name="player 1") {
     const _name = name;
     const _marker = marker;
     let _turn = [false];
-    const _cells = [];
+    let _cells = [];
     const play = (index, cellElem) => {
         Board.grid[index][2] = cellElem;
         _cells.push(Board.grid[index]);
@@ -13,6 +13,7 @@ const Player = function(marker, name="player 1") {
     const isPlayerTurn = () => _turn[0];
     const togglePlayerTurn = () => {_turn[0] = true};
     const getCells = () => _cells;
+    const reset = () => {_cells = []}
     return {
          play,
          getPlayerName,
@@ -20,6 +21,7 @@ const Player = function(marker, name="player 1") {
          togglePlayerTurn,
          getPlayerMarker,
          getCells,
+         reset,
         }
 }
 
@@ -56,8 +58,9 @@ const Board = (function(size=3) {
 
 const ticTacToe = (function(Player, Board) {
     //Create players for a 2 player game
-    const player1 = Player("O", "P1");
-    const player2 = Player("X", "P2");
+    let player1 = Player("O", "P1");
+    let player2 = Player("X", "P2");
+    let isGameOver = false;
 
     //Select dom elements that are required for manipulation
     const _domCache = {
@@ -67,6 +70,7 @@ const ticTacToe = (function(Player, Board) {
         grid: game.querySelector('#grid'),
         tds : grid.querySelectorAll("td"),
         restart: game.querySelector('#restart'),
+        multiplayer: game.querySelector("#ismultiplayer"),
     };
 
     //Display  player moves in the grid
@@ -89,19 +93,16 @@ const ticTacToe = (function(Player, Board) {
     const play = function(cellElem) {
         const cellIndex = parseInt(cellElem.dataset["cellIndex"]);
         const cell = Board.grid[cellIndex];
-        if (!(Board.isFreeCell(cell))) {
+        if ( isGameOver || !(Board.isFreeCell(cell)) ) {
             return
         }
         const player = _getPlayer();
         player.play(cellIndex, cellElem);
         _render(cellElem, player.getPlayerMarker());
         if (player.getCells().length > 2) {
-            if (endGame(player.getCells())) {
-                alert(`game over ${player.name} won`);
-            }
+            endGame(player.getCells(), player.getPlayerName());
         }
     };
-
 
     const _linearEquations = function(arr) {
         for (let i = 0; i < 3; i++) {
@@ -138,11 +139,13 @@ const ticTacToe = (function(Player, Board) {
             }
         }
     }
-    const endGame = function (arr) {
+    const endGame = function (arr, name) {
         if (_linearEquations(arr)) {
-            console.log("win");
+            alert(`game over ${name} won`)
+            isGameOver = true
         } else if (Board.getFreeCells().length < 1) {
-            console.log("draw");
+            alert("draw");
+            isGameOver = true
         }
     }
 
@@ -159,12 +162,26 @@ const ticTacToe = (function(Player, Board) {
     _resizeToSquare();
 
     const _restart = function() {
+        player1.reset();
+        player2.reset();
         Board.grid = Board.getGrid();
         _domCache.tds.forEach((td)=> _render(td, '.'));
     }
 
     _domCache.tds.forEach(element => {
-       element.addEventListener("click", (e)=>{play(e.target)})
+       element.addEventListener("click", (e)=>{
+        play(e.target)
+        if (!(_domCache.multiplayer.checked) && (Board.getFreeCells().length > 0)) {
+            let randomElem = null;
+            while (randomElem == null) {
+                const randomInndex = parseInt(Math.random() * Board.grid.length);
+                if (Board.isFreeCell(Board.grid[randomInndex])) {
+                    randomElem = _domCache.tds[randomInndex];
+                }
+            }
+            setTimeout(()=>{play(randomElem)}, 300);
+        }
+    })
     });
 
     window.addEventListener("resize", _resizeToSquare);
